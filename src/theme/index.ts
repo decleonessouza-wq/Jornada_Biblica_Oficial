469e8dd3f64f87e20c757c892221561b7e73ce1c
DECLEONES <decleones@andradecalhas.com.br>
2026-08-13 21:17:37 -0400

    feat: centralizar exports do design system

---
+++ src/theme/index.ts
@@ -0,0 +1,25 @@
+export { colors } from "./colors";
+export type { ColorToken } from "./colors";
+
+export { layout, spacing } from "./spacing";
+export type { LayoutToken, SpacingToken } from "./spacing";
+
+export {
+  fontSizes,
+  fontWeights,
+  letterSpacing,
+  lineHeights,
+  textStyles,
+} from "./typography";
+export type {
+  FontSizeToken,
+  FontWeightToken,
+  TextStyleToken,
+} from "./typography";
+
+export { borderWidths, elevation, radii } from "./shape";
+export type {
+  BorderWidthToken,
+  ElevationToken,
+  RadiusToken,
+} from "./shape";
